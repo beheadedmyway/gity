f755cb1f7c8412a570759fa3055df882516347ed
Brandon Sneed <brandon@redf.net>
2010-12-28 23:08:43 -0800

    some more outline view work.

--- src/GTGitFile.m
+++ src/GTGitFile.m
@@ -285,6 +285,17 @@
 	[super dealloc];
 }
 
+- (BOOL)isEqual:(id)object
+{
+	if (![object isKindOfClass:[GTGitFile class]])
+		return NO;
+
+	GTGitFile *otherFile = (GTGitFile *)object;
+	if (otherFile.filename)
+		return [self.filename isEqualToString:otherFile.filename];
+	return NO;
+}
+
 @end
 
 

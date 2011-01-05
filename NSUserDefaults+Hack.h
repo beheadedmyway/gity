d637d2708c959fae66b009f7040af59fb2fc1bd7
Brandon Sneed <brandon@redf.net>
2011-01-04 22:33:18 -0800

    Added NSUserDefaults hack to cleanup the junk plist files. :(

---
+++ NSUserDefaults+Hack.h
@@ -0,0 +1,14 @@
+//
+//  NSUserDefaults+Hack.h
+//  gitty
+//
+//  Created by brandon on 1/4/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface NSUserDefaults(Hack)
+
+@end
